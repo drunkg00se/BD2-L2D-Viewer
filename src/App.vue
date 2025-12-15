df109da335049263209ec692a9b1daf966cf2179
ddrunkgoose <ddrunkgoose@protonmail.com>
2025-12-16 05:06:51 +0800

    Move the AnimationSidebar to the right side

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,6 @@
       @overlay-active="onNavbarOverlayActive"
     />
     <div class="flex flex-1 flex-col lg:flex-row h-full min-h-0 overflow-hidden">
-      <div class="order-1 lg:order-none hidden lg:flex flex-col min-h-0">
-        <AnimationSidebar
-          :animations="animations"
-          :skins="skins"
-          :exporting="isExporting"
-          :screenshotting="isScreenshotting"
-          @select="onSelectAnimation"
-          @reset-camera="onResetCamera"
-          @screenshot="onScreenshot"
-          @export-animation="onExportAnimation"
-          @category-change="onCategoryChange"
-          class="lg:w-64"
-        />
-      </div>
       <main class="relative flex-1 p-2 overflow-hidden">
         <div class="absolute top-2 left-2 lg:hidden z-50 flex items-center gap-2">
           <button v-show="!overlayActive" class="p-2" @click="showMobileControls = true">
@@ -29,11 +15,7 @@
           <button v-show="!overlayActive" class="p-2" @click="onResetCamera">
             <CameraResetIcon />
           </button>
-          <button
-            v-show="!overlayActive"
-            class="p-2"
-            @click="store.playing = !store.playing"
-          >
+          <button v-show="!overlayActive" class="p-2" @click="store.playing = !store.playing">
             <PauseIcon v-if="store.playing" />
             <PlayIcon v-else />
           </button>
@@ -54,7 +36,11 @@
         </div>
         <div class="absolute top-14 left-4 lg:hidden z-50">
           <button
-            v-show="!overlayActive && store.characters.find(c => c.id === store.selectedCharacterId)?.datingHasNoBg && store.animationCategory === 'dating'"
+            v-show="
+              !overlayActive &&
+              store.characters.find((c) => c.id === store.selectedCharacterId)?.datingHasNoBg &&
+              store.animationCategory === 'dating'
+            "
             @click="store.showDatingBg = !store.showDatingBg"
           >
             <BgToggleIcon :active="store.showDatingBg" />
@@ -68,22 +54,25 @@
         />
       </main>
       <div class="hidden lg:flex flex-col min-h-0">
-        <CharacterSidebar
-          @select="onSelectCharacter"
-          class="lg:w-80"
+        <CharacterSidebar @select="onSelectCharacter" class="lg:w-80" />
+      </div>
+      <div class="order-1 lg:order-none hidden lg:flex flex-col min-h-0">
+        <AnimationSidebar
+          :animations="animations"
+          :skins="skins"
+          :exporting="isExporting"
+          :screenshotting="isScreenshotting"
+          @select="onSelectAnimation"
+          @reset-camera="onResetCamera"
+          @screenshot="onScreenshot"
+          @export-animation="onExportAnimation"
+          @category-change="onCategoryChange"
+          class="lg:w-64"
         />
       </div>
     </div>
-    <div
-      v-if="showMobileControls"
-      class="fixed inset-0 z-20 bg-gray-900 lg:hidden flex flex-col"
-    >
-      <button
-        class="absolute top-3 right-4"
-        @click="showMobileControls = false"
-      >
-        ✕
-      </button>
+    <div v-if="showMobileControls" class="fixed inset-0 z-20 bg-gray-900 lg:hidden flex flex-col">
+      <button class="absolute top-3 right-4" @click="showMobileControls = false">✕</button>
       <div class="flex-1 flex flex-col gap-2 overflow-hidden p-2">
         <div class="flex-none">
           <AnimationSidebar
@@ -106,73 +95,79 @@
 </template>
 
 <script setup lang="ts">
-import Navbar from '@/components/Navbar.vue'
-import CharacterSidebar from '@/components/CharacterSideBar.vue'
-import AnimationSidebar from '@/components/AnimationSideBar.vue'
-import SpineViewer from '@/components/SpineViewer.vue'
-import { ref, watchEffect, computed } from 'vue'
-import { useCharacterStore } from '@/stores/characterStore'
-import { buildUrl } from './utils/urlSync'
+import Navbar from "@/components/Navbar.vue";
+import CharacterSidebar from "@/components/CharacterSideBar.vue";
+import AnimationSidebar from "@/components/AnimationSideBar.vue";
+import SpineViewer from "@/components/SpineViewer.vue";
+import { ref, watchEffect, computed } from "vue";
+import { useCharacterStore } from "@/stores/characterStore";
+import { buildUrl } from "./utils/urlSync";
 
-import CameraResetIcon from '@/components/icons/CameraResetIcon.vue';
-import MenuIcon from '@/components/icons/MenuIcon.vue';
-import PauseIcon from '@/components/icons/PauseIcon.vue';
-import PlayIcon from '@/components/icons/PlayIcon.vue';
-import BgToggleIcon from '@/components/icons/BgToggleIcon.vue';
+import CameraResetIcon from "@/components/icons/CameraResetIcon.vue";
+import MenuIcon from "@/components/icons/MenuIcon.vue";
+import PauseIcon from "@/components/icons/PauseIcon.vue";
+import PlayIcon from "@/components/icons/PlayIcon.vue";
+import BgToggleIcon from "@/components/icons/BgToggleIcon.vue";
 
-const store = useCharacterStore()
+const store = useCharacterStore();
 
-const animations = ref<string[]>([])
-const skins = ref<string[]>([])
-const viewerRef = ref<InstanceType<typeof SpineViewer> | null>(null)
-const isExporting = ref(false)
-const isScreenshotting = ref(false)
-const showMobileControls = ref(false)
-const navMobileMenuOpen = ref(false)
-const navbarOverlayActive = ref(false)
+const animations = ref<string[]>([]);
+const skins = ref<string[]>([]);
+const viewerRef = ref<InstanceType<typeof SpineViewer> | null>(null);
+const isExporting = ref(false);
+const isScreenshotting = ref(false);
+const showMobileControls = ref(false);
+const navMobileMenuOpen = ref(false);
+const navbarOverlayActive = ref(false);
 const overlayActive = computed(
-  () => showMobileControls.value || navMobileMenuOpen.value || navbarOverlayActive.value,
-)
-const hasCustomBackground = computed(() => !!store.customBackgroundImage)
+  () => showMobileControls.value || navMobileMenuOpen.value || navbarOverlayActive.value
+);
+const hasCustomBackground = computed(() => !!store.customBackgroundImage);
 
 function onSelectCharacter(id: string) {
-  if (id === store.selectedCharacterId) return
-  store.selectedCharacterId = id
-  store.selectedAnimation = ''
-  store.selectedSkin = ''
-  animations.value = []
-  skins.value = []
-  showMobileControls.value = false
+  if (id === store.selectedCharacterId) return;
+  store.selectedCharacterId = id;
+  store.selectedAnimation = "";
+  store.selectedSkin = "";
+  animations.value = [];
+  skins.value = [];
+  showMobileControls.value = false;
 }
 
 function onSelectAnimation(name: string) {
-  store.selectedAnimation = name
-  showMobileControls.value = false
+  store.selectedAnimation = name;
+  showMobileControls.value = false;
 }
 
 function onResetCamera() {
-  viewerRef.value?.resetCamera()
+  viewerRef.value?.resetCamera();
 }
 
 function onScreenshot(value: boolean) {
-  if (!viewerRef.value) return
-  showMobileControls.value = false
-  showMobileControls.value = false
-  isScreenshotting.value = true
-  viewerRef.value.saveScreenshot(value)
-  isScreenshotting.value = false
+  if (!viewerRef.value) return;
+  showMobileControls.value = false;
+  showMobileControls.value = false;
+  isScreenshotting.value = true;
+  viewerRef.value.saveScreenshot(value);
+  isScreenshotting.value = false;
 }
 
-async function onExportAnimation({ format, transparent }: { format: 'video' | 'frames'; transparent: boolean }) {
-  if (!viewerRef.value) return
-  showMobileControls.value = false
-  isExporting.value = true
-  if (format === 'frames') {
-    await viewerRef.value.exportAnimationFrames(transparent)
+async function onExportAnimation({
+  format,
+  transparent,
+}: {
+  format: "video" | "frames";
+  transparent: boolean;
+}) {
+  if (!viewerRef.value) return;
+  showMobileControls.value = false;
+  isExporting.value = true;
+  if (format === "frames") {
+    await viewerRef.value.exportAnimationFrames(transparent);
   } else {
-    await viewerRef.value.exportAnimation(transparent)
+    await viewerRef.value.exportAnimation(transparent);
   }
-  isExporting.value = false
+  isExporting.value = false;
 }
 
 function onCategoryChange() {
@@ -180,22 +175,25 @@ function onCategoryChange() {
 }
 
 function onNavMobileMenu(open: boolean) {
-  navMobileMenuOpen.value = open
+  navMobileMenuOpen.value = open;
 }
 
 function onCustomBgUpload(image: string | null) {
   if (image && image === store.customBackgroundImage) {
-    store.customBackgroundImage = null
+    store.customBackgroundImage = null;
   }
-  store.customBackgroundImage = image
+  store.customBackgroundImage = image;
 }
 
 function onNavbarOverlayActive(active: boolean) {
-  navbarOverlayActive.value = active
+  navbarOverlayActive.value = active;
 }
 
-watchEffect(() => {
-  const query = buildUrl(store)
-  history.replaceState(null, '', `${window.location.pathname}?${query}`)
-}, { flush: 'sync' })
+watchEffect(
+  () => {
+    const query = buildUrl(store);
+    history.replaceState(null, "", `${window.location.pathname}?${query}`);
+  },
+  { flush: "sync" }
+);
 </script>
